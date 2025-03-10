3a5f404bf8afb66592f404b89041cc47447658d6
Unknowncodeguy010 <164704925+Unknowncodeguy010@users.noreply.github.com>
2025-03-10 13:44:31 -0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: 'gaara',
+  author: 'Corrupt',
+  botName: "Gaarakun",
+  botOwner: 'Corruptmvn', // Your name
+  ownerNumber: '18763122982', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
